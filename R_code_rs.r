43ddfe5ec8cb046d261909c3a97fca57e09af0a0
Giovanni2125 <100956801+Giovanni2125@users.noreply.github.com>
2022-03-10 17:55:46 +0100

    Update R_code_rs.r

--- R_code_rs.r
+++ R_code_rs.r
@@ -1,3 +1,13 @@
 Il primo script di telerilevamento
 
+#install packages (raster)
 library(raster)
+
+#settaggio cartella di lavoro
+
+setwd("C:/Users/Giovanni/Desktop/lab")
+#importare l'immagine
+landsat2011<- brick("C:/Users/Giovanni/Desktop/lab/p224r63_2011.grd")
+#plot 
+plot(landsat2011)
+cl<- colorRampPalette(c("black", "grey", "light grey")) (100)
