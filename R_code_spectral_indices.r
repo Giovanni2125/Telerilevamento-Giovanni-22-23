4fe73765077002200c2c6e23d5099d9ae38392b7
Giovanni2125 <100956801+Giovanni2125@users.noreply.github.com>
2022-04-03 13:27:58 +0200

    Update R_code_spectral_indices.r

--- R_code_spectral_indices.r
+++ R_code_spectral_indices.r
@@ -11,6 +11,7 @@ setwd("C:/Users/Giovanni/Desktop/lab")
 l1992<-brick ("C:\Users\Giovanni\Desktop\lab\defor1_.jpg")
 
 plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+ plot(ndvi1992, col=cl)
 
 #layer1=NIR
 #layer2=red
@@ -55,8 +56,72 @@ plot(difdvi, col=cld)
 
 #day 2
 
-# RANGEdvi 8 bit 255 -2555
+# RANGEdvi 8 bit  -255, 255
 #RANGE NDVI 1 -1
-#Range DVI (16 bit) 65535 -65535
+#Range DVI (16 bit) -65535, 65535
 #Range NDVI 1 -1
 #NDVI può esserer usato anche con immagini differenti con risoluzione radiometrica differente
+
+library(raster)
+
+setwd("C:/Users/Giovanni/Desktop/lab")
+
+#import the two files defor_1 and defor_2
+
+l1992<- brick("C:/Users/Giovanni/Desktop/lab/defor1_.jpg")
+
+l2006<- brick("C:/Users/Giovanni/Desktop/lab/defor2_.jpg")
+
+#calcolo DVI per calcolarlo usiamo solo NIR e red
+#riflettanza in NIR-riflettanza in red
+
+#components
+#NDVI 1992
+dvi1992 = l1992[[1]] - l1992[[2]]
+ndvi1992 = dvi1992 / l1992[[1]] + l1992[[2]]
+
+cl <- colorRampPalette(c('darkblue','yellow','red','black'))(100)
+plot(ndvi1992, col=cl)
+#multiframe with plot RGB up and ndvi1992 under
+
+par(mfrow=c(2,1))
+
+plotRGB(l1992, r=1, g=2, b=3, stretch="lin")
+plot(ndvi1992, col=cl)
+
+#2006
+dvi2006 = l2006[[1]] - l2006[[2]]
+
+ndvi2006 = dvi2006 / (l2006[[1]] + l2006[[2]])
+
+#multiframe with ndvi1992 on top of the NDVI 2006
+
+par(mfrow=c(2,1))
+
+plot(ndvi1992, col=cl)
+
+plot(ndvi2006, col=cl)
+
+#install package RStoolbox
+
+library(RStoolbox)
+
+#automatic spectral indices by the spectralIndices function
+
+si1992 <- spectralIndices(l1992, green=3, red=2, nir=1)
+
+plot(si1992,col=cl)
+
+#same thing with 2006
+
+si2006 <- spectralIndices(l2006, green=3, red=2, nir=1)
+
+plot(si2006,col=cl)
+
+#install package rasterdiv
+install.packages("rasterdiv")
+
+library(rasterdiv)
+
+#rasterdiv
+plot(copNDVI)
